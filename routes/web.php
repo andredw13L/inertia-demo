4d2a1500044e39d72fcbea1545e88ba9747bacd1
andredw13L <andreribeiro13l@gmail.com>
2025-10-22 03:28:41 -0300

    Pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home', [
+        'name' => 'André Ribeiro',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+        ]
+    ]);
 });
