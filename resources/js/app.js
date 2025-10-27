6804d58076b98774bd395ff5fbde80c2c783cd0d
andredw13L <andreribeiro13l@gmail.com>
2025-10-27 20:54:18 -0300

    Code Splitting and Dynamic Imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,15 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import Layout from './Shared/Layout.vue'
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    let page = pages[`./Pages/${name}.vue`]
+  resolve: async (name) => {
+    
+    const pages = import.meta.glob('./Pages/**/*.vue', {eager: false})
+
+    let page = await pages[`./Pages/${name}.vue`]()
+
     page.default.layout = page.default.layout || Layout
-    return page
+
+    return page;
   },
 
   setup({ el, App, props, plugin }) {
